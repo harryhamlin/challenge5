9324d96e22ef9638026b3e9cdf0ed463aa428294
bulk <harryhamlin@gmail.com>
2022-12-10 17:19:01 -0800

    added function to collect the information in any given text area relevant to the specific save button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,30 +13,27 @@ $(function () {
     }, 1000)
   }
 
-  // the createTimeSlotDisplay creates a series of timeslots through DOM manipulation, accounting for the AM/PM time display
+  // the createTimeSlotDisplay creates a series of timeslots through DOM manipulation, accounting for the AM/PM time display. it sets variabls hourcheck and current hour to use in the later function checkIfPPF to see if the current time slot is past present or future.
   function createTimeSlotDisplay() {
     for (let i = 0; i < 25; i++) {
-      let hourCheck = i;
       let currentHour = parseInt(dayjs().format('H'));
       let ppf = checkIfPPF();
-      //createTimeSlot is declared, the creation of the slots through DOM manipulation
+      // createTimeSlot() is declared, the creation of the slots through DOM manipulation
       function createTimeSlot(ppf, time, meridiem) {
-        $('#main').append('<div id="hour-' + (hourCheck - 1) + '" class="row time-block ' + ppf + '"><div class="col-2 col-md-1 hour text-center py-3">' + time + meridiem + '</div><textarea class="col-8 col-md-10 description" rows="3"> </textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button></div>');
+        $('#main').append('<div id="hour-' + i + '" class="row time-block ' + ppf + '"><div class="col-2 col-md-1 hour text-center py-3">' + time + meridiem + '</div><textarea id="text-area-' + i + '" class="col-8 col-md-10 description" rows="3"> </textarea><button id="' + i + '" class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true" id="' + i + '"></i></button></div>');
       }
-      
+      // checkIfPFF() is declared, checking if each slot is before, during or after the present time
       function checkIfPPF() {
-        console.log(hourCheck);
-        console.log(currentHour);
-        if (hourCheck < currentHour) {
+        if (i < currentHour) {
           return 'past';
         } else 
-        if (hourCheck === currentHour) {
+        if (i === currentHour) {
           return 'present';
         } else {
           return 'future';
         }
       }
-      
+
       // a series of if statements accounting for AM v PM, hour 12 v not hour 12
       if (i === 0) {
         let time = 12;
@@ -58,21 +55,21 @@ $(function () {
     }
   }
 
-
-
+  $('#main').on('click',function (event) {
+    let userClick = event.target.nodeName;
+    if (userClick === 'BUTTON' || userClick === 'I') {
+      let idNumberConcat = '#text-area-' + event.target.id;
+      let textContent = $(idNumberConcat).val();
+      
+    }
+  })
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -82,5 +79,4 @@ $(function () {
 
   dateDisplayFx();
   createTimeSlotDisplay();
-  console.log(dayjs().format('H'))
 });
